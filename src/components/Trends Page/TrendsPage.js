3e73e3e1c895ff39a752e4dbf0db5e0c8aad4421
Omer Ezra <90283262+lieudusty123@users.noreply.github.com>
2022-09-21 16:30:33 +0300

    add error answer when given bad query

--- src/components/Trends Page/TrendsPage.js
+++ src/components/Trends Page/TrendsPage.js
@@ -1,15 +1,10 @@
-import React, {
-  Fragment,
-  useContext,
-  useEffect,
-  useRef,
-  useState,
-} from "react";
+import React, { useContext, useEffect, useRef } from "react";
 import axios from "axios";
 import { Chart, registerables } from "chart.js";
 import context from "../../contextAPI/context";
 import SearchForm from "../UI/SearchForm";
 import { useLocation } from "react-router-dom";
+import classes from "./TrendsPageStyling/trendsPage.module.css";
 
 let chart = {};
 const TrendsPage = (props) => {
@@ -20,91 +15,96 @@ const TrendsPage = (props) => {
   let searchTerm = "";
   getInfoFromUrl();
   function getInfoFromUrl() {
-    console.log(location);
     const lastIndex = location.pathname.lastIndexOf("/");
     searchTerm = location.pathname.slice(lastIndex + 1);
-    console.log(searchTerm);
   }
   getInfoFromUrl();
 
   useEffect(() => {
     const fetchInterestOverTime = async () => {
       if (chart.id !== undefined) {
-        console.log("inside");
         chart.destroy();
-        console.log("chart", chart);
       }
-      console.log(chart);
-      console.log(data.initials);
-      console.log(searchTerm);
       const response = await axios.get(
         `/.netlify/functions/interestOverTime?country=${data.initials}&term=${searchTerm}`
       );
+      console.log(response);
       const responseData = await response.data;
-
-      let chartData = {
-        dates: [],
-        data: [],
-      };
-      let index = 0;
-      console.log(responseData);
-      responseData.default.timelineData.forEach((date) => {
-        if (date.value[0] > 50) {
-          if (responseData.default.timelineData[index].value[0] < 10) {
-            chartData.data.push(
-              responseData.default.timelineData[index].value[0]
-            );
+      if (
+        typeof responseData === "string" ||
+        responseData.default.timelineData.length === 0
+      ) {
+        return console.log(
+          "Oops! something went wrong! you might wanna double-check your query.."
+        );
+      } else {
+        let chartData = {
+          dates: [],
+          data: [],
+        };
+        let index = 0;
+        responseData.default.timelineData.forEach((date) => {
+          if (date.value[0] > 50) {
+            if (responseData.default.timelineData[index].value[0] < 10) {
+              chartData.data.push(
+                responseData.default.timelineData[index].value[0]
+              );
+            }
           }
-        }
-        chartData.data.push(date.value[0]);
-        chartData.dates.push(date.formattedTime);
-        index++;
-      });
+          chartData.data.push(date.value[0]);
+          chartData.dates.push(date.formattedTime);
+          index++;
+        });
 
-      let targetElement = pleaseWork.current;
-      let datas = {
-        labels: chartData.dates,
-        datasets: [
-          {
-            label: searchTerm,
-            data: chartData.data,
-            fill: false,
-            borderColor: "rgb(46, 123, 255)",
-            tension: 0.3,
-          },
-        ],
-      };
-      let config = {
-        type: "line",
-        data: datas,
-        options: {
-          scales: {
-            y: {
-              max: 100,
-              min: 0,
-              ticks: {
-                stepSize: 25,
+        let targetElement = pleaseWork.current;
+        let datas = {
+          labels: chartData.dates,
+          datasets: [
+            {
+              label: searchTerm,
+              data: chartData.data,
+              fill: false,
+              borderColor: "rgb(46, 123, 255)",
+              tension: 0.3,
+            },
+          ],
+        };
+        let config = {
+          type: "line",
+          data: datas,
+          options: {
+            scales: {
+              y: {
+                max: 100,
+                min: 0,
+                ticks: {
+                  stepSize: 25,
+                },
+              },
+              x: {
+                ticks: {
+                  autoSkip: true,
+                  maxTicksLimit: 5,
+                },
               },
             },
-            x: {
-              ticks: {
-                autoSkip: true,
-                maxTicksLimit: 5,
-              },
-            },
+            responsive: true,
           },
-        },
-      };
-      chart = new Chart(targetElement, config);
-      console.log(chart);
+        };
+        chart = new Chart(targetElement, config);
+      }
     };
     fetchInterestOverTime();
   }, [searchTerm, data.initials]);
   return (
-    <Fragment>
+    <div className={classes.root}>
       <SearchForm />
-      <canvas ref={pleaseWork}></canvas>
-    </Fragment>
+      <div className={classes.graphContainer}>
+        <canvas className={classes.graph} ref={pleaseWork}>
+          {chart.id === undefined && <div>something is fucked</div>}
+        </canvas>
+      </div>
+    </div>
   );
 };
 
--- src/components/UI/SearchForm.js
+++ src/components/UI/SearchForm.js
@@ -1,5 +1,5 @@
 import classes from "../Home Page/Header/HeaderStyling/header.module.scss";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useContext, useState } from "react";
 import context from "../../contextAPI/context";
 
