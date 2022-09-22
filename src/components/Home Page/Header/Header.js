777ebda9b6a4a2cd4fef261e8aa5ddca362a5e2f
Omer Ezra <90283262+lieudusty123@users.noreply.github.com>
2022-09-22 23:09:16 +0300

    fixed typewriter glitch

--- src/components/Home Page/Header/Header.js
+++ src/components/Home Page/Header/Header.js
@@ -5,49 +5,50 @@ import SearchForm from "../../UI/SearchForm";
 
 const Header = () => {
   const typeWriterEle = useRef();
-  let i = 0;
-  var txtArr = ["trends", "events", "searches", "stories"]; /* The text */
-  let currentWord = 0;
-  var speed = 100;
 
-  async function typeWriter() {
-    if (i < txtArr[currentWord].length) {
-      typeWriterEle.current.innerHTML += txtArr[currentWord].charAt(i);
-      i++;
-      setTimeout(typeWriter, speed);
-    } else if (currentWord < txtArr.length - 1) {
-      await sleep(1000);
-      setTimeout(removeTypeWriter, speed);
-    } else {
-      await sleep(1000);
-      setTimeout(removeTypeWriter, speed);
-    }
-  }
-  function sleep(ms) {
-    return new Promise((resolve) => setTimeout(resolve, ms));
-  }
-  function removeTypeWriter() {
-    if (i > 0) {
-      let pleaseWork = typeWriterEle.current.innerHTML;
-      typeWriterEle.current.innerHTML = pleaseWork.slice(
-        0,
-        pleaseWork.length - 1
-      );
-      i--;
-      setTimeout(removeTypeWriter, speed);
-    } else {
-      if (txtArr[currentWord] === txtArr[txtArr.length - 1]) {
+  useEffect(() => {
+    let i = 0;
+    var txtArr = ["trends", "events", "searches", "stories"]; /* The text */
+    let currentWord = 0;
+    var speed = 100;
+
+    async function typeWriter() {
+      if (i < txtArr[currentWord].length) {
+        typeWriterEle.current.innerHTML += txtArr[currentWord].charAt(i);
+        i++;
         setTimeout(typeWriter, speed);
-        currentWord = 0;
-        i = 0;
+      } else if (currentWord < txtArr.length - 1) {
+        await sleep(1000);
+        setTimeout(removeTypeWriter, speed);
       } else {
-        setTimeout(typeWriter, speed);
-        currentWord++;
-        i = 0;
+        await sleep(1000);
+        setTimeout(removeTypeWriter, speed);
       }
     }
-  }
-  useEffect(() => {
+    function sleep(ms) {
+      return new Promise((resolve) => setTimeout(resolve, ms));
+    }
+    function removeTypeWriter() {
+      if (i > 0) {
+        let pleaseWork = typeWriterEle.current.innerHTML;
+        typeWriterEle.current.innerHTML = pleaseWork.slice(
+          0,
+          pleaseWork.length - 1
+        );
+        i--;
+        setTimeout(removeTypeWriter, speed);
+      } else {
+        if (txtArr[currentWord] === txtArr[txtArr.length - 1]) {
+          setTimeout(typeWriter, speed);
+          currentWord = 0;
+          i = 0;
+        } else {
+          setTimeout(typeWriter, speed);
+          currentWord++;
+          i = 0;
+        }
+      }
+    }
     typeWriter();
   }, []);
   console.log(typeWriterEle.current);
